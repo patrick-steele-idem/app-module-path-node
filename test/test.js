62682b0d3ed96e9185398d63eeaadea5b989b5ac
Michael Rawlings <ml.rawlings@gmail.com>
2016-09-12 14:35:10 -0700

    fixes #20 - issue with a local module being found before an installed module is found

--- test/test.js
+++ test/test.js
@@ -1,8 +1,12 @@
 var path = require('path');
+var assert = require('assert');
 require('../').addPath(path.join(__dirname, 'src'));
 
 require('module-a').sayHello();
 require('module-b').sayHello();
+require('installed-module').sayHello();
+
+assert(require('installed-module.js').isLocal);
 
 require('package.json');
 

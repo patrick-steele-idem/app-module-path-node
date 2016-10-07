48748177b9df8908e1e2b62fa739fea717ae8d69
Adam Putinski <adam.putinski@gmail.com>
2016-10-07 17:52:30 -0400

    Add removePath()

--- test/test2.js
+++ test/test2.js
@@ -1,10 +1,19 @@
+var path = require('path');
+var assert = require('assert');
+
 require('./test-helper-code.js');
 
 describe("support for test code", function () {
-
   it("should load up a module in a path defined in test helper code", function () {
     require('module-a').sayHello();
     require('module-b').sayHello();
+  });
+  it("should not search paths that have been removed", function () {
+    require('module-c').sayHello();
+    require('../').removePath(path.join(__dirname, 'src'));
+    assert.throws(function () {
+      require('module-d').sayHello();
+    });
   });
 });
 
